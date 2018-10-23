a536edd56d153deeb9a0aeec6bbbd9cfc0f0952c
sun5hine <af5kh@yahoo.com>
2018-10-23 15:17:48 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,7 @@ def monopoly_with_fourth_tier
 	   :b_and_o_railroad => {:mortgage_value  => "$400"},
 	   :shortline => {:mortgage_value  => "$800"}, 
 	   :rent_in_dollars = {:one_piece_owned => 4, :two_piece_owned => 50, :three_piece_owned => 100, :four_piece_owned => 200},
-	  }  
+	   
 	  }
 	}
 	
